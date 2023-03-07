151243cfa61579db2802692196c3e15db6869c55
Yesith Alexander Alvarez Matta <56172878+yealvarez@users.noreply.github.com>
2023-03-07 14:31:03 -0500

    Add files via upload

---
+++ PasswordFinder.ps1
@@ -0,0 +1,3 @@
+﻿$Files = Get-ChildItem 'C:\Program Files\CIMCO 2022\*.*' -Recurse
+
+Get-Childitem $Files -Include *.json, *.txt, *.config, *.config, *.inc, *.prop, *.xml, *.sql -Recurse | Select-String -Pattern "password", "pwd", "user", "usr", "USER", "User", "API", "API_KEY","KEY"
